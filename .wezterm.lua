8bab5cf4e1f643c725843208da3eed8c0b153b3d
Louis Stanard <louis.stanard@gmail.com>
2025-02-21 13:14:41 -0600

    update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,10 @@ local act = wezterm.action
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 local pain_control = wezterm.plugin.require("https://github.com/sei40kr/wez-pain-control")
 
--- This will hold the configuration.
+-- Config
 local config = wezterm.config_builder()
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 -- Keys
 config.keys = {
@@ -18,30 +20,83 @@ config.keys = {
 	}
 }
 
--- This is where you actually apply your config choices
+-- UI config
+config.color_scheme = 'catppuccin-frappe' -- also trying out catppuccin-mocha for a change
+config.enable_tab_bar = true
+config.initial_cols = 180
+config.initial_rows = 60
+config.use_fancy_tab_bar = true
+config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = '12px',
 	right = '12px',
-	top = '12px',
+	top = '18px',
 	bottom = '12px',
 }
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
-config.color_scheme = 'catppuccin-mocha'
-config.font = wezterm.font_with_fallback {
-  { family = 'JetBrains Mono', weight = 'Regular' },
-  'Hack Nerd Font',
-}
-config.font_size = 13
-
--- Tab bar
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = true
-config.window_decorations = "RESIZE"
 
 -- Apply plugin config
-bar.apply_to_config(config)
+bar.apply_to_config(config, {
+  position = "bottom",
+  max_width = 32,
+  padding = {
+    left = 1,
+    right = 1,
+  },
+  separator = {
+    space = 1,
+    left_icon = wezterm.nerdfonts.fa_long_arrow_right,
+    right_icon = wezterm.nerdfonts.fa_long_arrow_left,
+    field_icon = wezterm.nerdfonts.indent_line,
+  },
+  modules = {
+    tabs = {
+      active_tab_fg = 4,
+      inactive_tab_fg = 6,
+    },
+    workspace = {
+      enabled = false,
+      icon = wezterm.nerdfonts.cod_window,
+      color = 8,
+    },
+    leader = {
+      enabled = true,
+      icon = wezterm.nerdfonts.oct_rocket,
+      color = 2,
+    },
+    pane = {
+      enabled = true,
+      icon = wezterm.nerdfonts.cod_multiple_windows,
+      color = 7,
+    },
+    username = {
+      enabled = true,
+      icon = wezterm.nerdfonts.fa_user,
+      color = 6,
+    },
+    hostname = {
+      enabled = false,
+      icon = wezterm.nerdfonts.cod_server,
+      color = 8,
+    },
+    clock = {
+      enabled = true,
+      icon = wezterm.nerdfonts.md_calendar_clock,
+      color = 5,
+    },
+    cwd = {
+      enabled = true,
+      icon = wezterm.nerdfonts.oct_file_directory,
+      color = 7,
+    },
+    spotify = {
+      enabled = false,
+      icon = wezterm.nerdfonts.fa_spotify,
+      color = 3,
+      max_width = 64,
+      throttle = 15,
+    },
+  },
+})
 pain_control.apply_to_config(config, {})
 
--- and finally, return the configuration to wezterm
 return config
-
